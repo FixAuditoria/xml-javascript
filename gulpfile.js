f6384668a63a0812e0161cbef86faf7c482e283b
Walber Jefferson <walberjefferson@hotmail.com>
2018-08-30 16:42:48 -0300

    Compilado para producao

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const elixir = require('laravel-elixir');
 
 elixir((mix) => {
     mix.webpack('app.js', './assets/js/app.js');
-    mix.scripts(['script.js', 'upload.js'], './assets/js/script.js');
+    //mix.scripts(['script.js', 'upload.js'], './assets/js/script.js');
 
     mix.sass('app.scss', './assets/css/app.css');
     mix.sass('primer-tooltips.scss', './assets/css/primer-tooltips.css');
